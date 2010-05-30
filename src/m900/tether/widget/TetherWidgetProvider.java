4878f7a10ded5200053f37717d1fc7a38b429e3a
caspian <srtelle@gmail.com>
2010-05-30 01:10:20 -0400

    Add toggle widget.

---
+++ src/m900/tether/widget/TetherWidgetProvider.java
@@ -0,0 +1,175 @@
+package m900.tether.widget;
+
+import java.io.IOException;
+
+import m900.tether.R;
+import m900.tether.system.CoreTask;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class TetherWidgetProvider extends AppWidgetProvider
+{
+
+    /**
+     * Action to pass in for the intent
+     */
+    private static final String ACTION_WIDGET_RECEIVER = "ActionRecieverWidget";  
+
+    /**
+     * Key to get the tethering status
+     */
+    public static final String TETHER_STATUS = "tether.status";
+    
+    /**
+     * Value of tether.status that means the phone is currently tethering
+     */
+    public static final String TETHER_ENABLED = "running";
+    
+    @Override  
+    public void onUpdate(
+        Context context, 
+        AppWidgetManager appWidgetManager, 
+        int[] appWidgetIds)    
+    {
+        
+        // create an intent
+        Intent intent = new Intent(context, TetherWidgetProvider.class);
+        intent.setAction(ACTION_WIDGET_RECEIVER);
+        
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+            context, 0, intent, 0);
+        RemoteViews views = new RemoteViews(
+            context.getPackageName(), R.layout.widgetview);
+        
+        views.setOnClickPendingIntent(R.id.buttonWidget, pendingIntent);
+
+        // set the widget button accordingly
+        if (isTetheringEnabled())
+        {
+            views.setImageViewResource(R.id.buttonWidget, R.drawable.icon); 
+        }
+        else
+        {
+            views.setImageViewResource(R.id.buttonWidget, R.drawable.icon_off); 
+        }
+
+        for (int i = 0; i < appWidgetIds.length; i++) 
+        {  
+            int appWidgetId = appWidgetIds[i];  
+            appWidgetManager.updateAppWidget(appWidgetId, views);  
+        }  
+    }
+
+    @Override  
+    public void onReceive(Context context, Intent intent) 
+    {  
+        if (intent.getAction().equals(ACTION_WIDGET_RECEIVER)) 
+        {  
+            // get the views
+            RemoteViews views = new RemoteViews(
+                context.getPackageName(),  
+                R.layout.widgetview);  
+
+            // are we tethering now?
+            if (isTetheringEnabled())
+            {
+                // stop tethering
+                stopTethering();
+            }
+            else
+            {
+                // start tethering
+                startTethering();
+            }
+            
+            // check the status - to change the button icon
+            if (isTetheringEnabled())
+            {
+                views.setImageViewResource(R.id.buttonWidget, R.drawable.icon); 
+            }
+            else
+            {
+                views.setImageViewResource(R.id.buttonWidget, R.drawable.icon_off); 
+            }
+            
+            // get the component and call update on it
+            ComponentName cn = new ComponentName(context, TetherWidgetProvider.class);  
+            AppWidgetManager.getInstance(context).updateAppWidget(cn, views);  
+        }
+
+        super.onReceive(context, intent);  
+    }
+
+    /**
+     * Start tethering using the command tether start 1
+     */
+    private void startTethering() 
+    {
+        String cmd[] = new String[3];
+        cmd[0] = "su";
+        cmd[1] = "-c";
+        cmd[2]= "/data/data/m900.tether/bin/tether start 1";
+
+        // get the runtime object
+        Runtime r = Runtime.getRuntime();
+        try
+        {
+            Process p = r.exec(cmd);
+            p.waitFor();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }     
+        
+    }
+
+    /**
+     * stop tethering using stop 1
+     */
+    private void stopTethering() 
+    {
+        String cmd[] = new String[3];
+        cmd[0] = "su";
+        cmd[1] = "-c";
+        cmd[2]= "/data/data/m900.tether/bin/tether stop 1";
+
+        // get the runtime object
+        Runtime r = Runtime.getRuntime();
+        try
+        {
+            Process p = r.exec(cmd);
+            p.waitFor();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }     
+        
+    }
+    
+    /**
+     * Return wether or not the phone is currently tethering
+     * 
+     * @return
+     */
+    public boolean isTetheringEnabled()
+    {
+        CoreTask coretask = new CoreTask();
+        coretask.setPath("/data/data/m900.tether");
+        return coretask.getProp(TETHER_STATUS).equals(TETHER_ENABLED);
+    }
+}
